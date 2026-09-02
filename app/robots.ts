d10be7a7199aa01d31402d04ba3b7aef43021c14
HB <134529662+Himeshbror18@users.noreply.github.com>
2026-09-02 12:43:31 +0530

    feat: add robots policy

---
+++ app/robots.ts
@@ -0,0 +1,2 @@
+import type {MetadataRoute} from 'next'
+export default function robots():MetadataRoute.Robots{return{rules:{userAgent:'*',allow:'/'},sitemap:'https://vector-lab-himeshbror-5266s-projects.vercel.app/sitemap.xml'}}
