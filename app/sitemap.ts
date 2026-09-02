1e47ada380002982a9f5716cfbb33c4b88b640f2
HB <134529662+Himeshbror18@users.noreply.github.com>
2026-09-02 12:43:26 +0530

    feat: add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,2 @@
+import type {MetadataRoute} from 'next'
+export default function sitemap():MetadataRoute.Sitemap{return[{url:'https://vector-lab-himeshbror-5266s-projects.vercel.app',lastModified:new Date(),changeFrequency:'weekly',priority:1}]}
